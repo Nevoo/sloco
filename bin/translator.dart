2e2374047f8762344cff3174db00af64290f67e7
Rouven Lührs <rouven.luehrs@bytabo.de>
2022-03-26 14:53:38 +0100

    added system locale as default base locale

--- bin/translator.dart
+++ bin/translator.dart
@@ -1,34 +1,25 @@
 import 'package:args/args.dart';
+import 'package:translator/core/classes/commands.dart';
 import 'package:translator/translator.dart' as translator;
-
-/// ARGS:
-// -default-language
-// -project-path
-// -language-file-path
-// -language-codes
-// -use-deepl
-
-class Commands {
-  const Commands._();
-
-  static const defaultLanguage = 'default-language';
-  static const projectPath = 'project-path';
-  static const languageFilesPath = 'languageFilesPath';
-  static const languageCodes = 'languageCodes';
-  static const useDeepl = 'use-deepl';
-}
+import 'dart:io' show Platform;
 
 void main(List<String> arguments) {
   final parser = ArgParser()
-    ..addFlag(Commands.useDeepl, abbr: 'u', defaultsTo: true)
-    // TODO: get system language as default
+    ..addFlag(
+      Commands.useDeepl,
+      abbr: 'u',
+      defaultsTo: true,
+      help: 'Translate all texts via the DeepL API',
+    )
     ..addOption(
       Commands.defaultLanguage,
       abbr: 'd',
       valueHelp: 'de',
-      defaultsTo: 'de',
+      defaultsTo: Platform.localeName,
+      help: 'Set your default project language',
     );
 
+  print(parser.usage);
   ArgResults argResults = parser.parse(arguments);
 
   print(argResults.rest);
