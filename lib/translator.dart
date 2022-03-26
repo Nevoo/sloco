c65a25d652e08ad8ef3d5aa2d0d631affb246c90
Rouven Lührs <rouven@luehrs.dev>
2022-03-26 23:20:10 +0100

    added code documentation to the translate function

--- lib/translator.dart
+++ lib/translator.dart
@@ -23,6 +23,9 @@ class Translator {
     required this.env,
   });
 
+  /// Searching for all Strings in the project that use the `.tr` extension.
+  /// Creates language files based on the provided language codes
+  /// and translates via DeepL if wanted.
   void translate() async {
     final fileNamesWithTranslation = await _getFileNamesWithTranslations();
     await _writeTranslationsToBaseFile(fileNamesWithTranslation);
