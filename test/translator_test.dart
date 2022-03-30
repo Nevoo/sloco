4d60e605be65bbc6c42a3c9127296d220d75bd10
Rouven Lührs <rouven@luehrs.dev>
2022-03-30 15:07:43 +0200

    implemented a test case for the translate function

--- test/translator_test.dart
+++ test/translator_test.dart
@@ -1,6 +1,37 @@
+import 'dart:convert' show utf8;
+import 'dart:io' show File;
+
+import 'package:translator/env/env.dart';
 import 'package:translator/translator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {});
+  test('Calls translate and generates base_translation file', () async {
+    final translator = Translator(
+      defaultLanguage: 'de',
+      useDeepL: false,
+      env: Env(),
+    );
+
+    expect(translator.useDeepL, isFalse);
+    expect(translator.defaultLanguage, equals('de'));
+
+    await translator.translate();
+
+    var baseTranslationFile = File(
+      'lib/locale/translations/base_translations/de.dart',
+    );
+
+    var contentStream = baseTranslationFile.openRead();
+    var decoded = await utf8.decodeStream(contentStream);
+
+    var expectedResult = r'''final Map<String, String> de = {
+
+	//  translator.dart
+	'Dusche': 'Dusche',
+};
+''';
+
+    expect(expectedResult, equals(decoded));
+  });
 }
